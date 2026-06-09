e0c9450249785d8d5ea0178ada6d14fc5dd57328
AntonyAkash <antoney97@gmail.com>
2026-06-09 11:18:07 +0530

    Add XML configuration files and update .gitignore; minor comment change in SpringDataJdbcApplication
    (cherry picked from commit 408a252b5ad28848c1a7e94f83945e8750f9ba87)

--- SpringDataJDBCDemo/SpringDataJDBC/src/main/java/com/example/demo/SpringDataJdbcApplication.java
+++ SpringDataJDBCDemo/SpringDataJDBC/src/main/java/com/example/demo/SpringDataJdbcApplication.java
@@ -9,12 +9,8 @@ public class SpringDataJdbcApplication {
 	//Hi Vine How are you
 	//I am goog antony
 
-	//this Antony branch
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJdbcApplication.class, args);
 	}
-
-	//method that is fixed in this branch and need for the current Story
 
 }
